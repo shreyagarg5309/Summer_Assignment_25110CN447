65178bee54823bfa44646cd719dcdba721ab74ea
shreyagarg5309 <shreyagarg5309@gmail.com>
2026-06-05 12:25:44 +0530

    Add files via upload

---
+++ Day9/Q35.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    char letter;
+
+    printf("Enter the last letter of rows: ");
+    scanf("%c", &letter);
+
+    for(char i = 'A'; i <= letter; i++){
+        for(char j = 'A'; j <= i; j++){
+            printf("%c ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day9/Q36.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Enter the no of rows: ");
+    scanf("%d", &num);
+
+    printf("\n");
+
+    printf("*****\n");
+
+    for(int i = 1; i <= num; i++){
+        printf("*   *\n");  
+    }
+
+    printf("*****\n");
+    printf("\n");
+
+    return 0;
+}
