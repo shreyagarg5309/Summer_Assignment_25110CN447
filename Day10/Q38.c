5d6b2e2d0e0f5a755fdd5d47a4d34d67fff758d1
shreyagarg5309 <shreyagarg5309@gmail.com>
2026-06-06 11:50:32 +0530

    Add files via upload

---
+++ Day10/Q38.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Enter the no of rows: ");
+    scanf("%d", &num);
+
+    for(int i = num; i >= 1; i--){
+        for(int j = 1; j <= num - i; j++){
+            printf(" ");
+        }
+
+        for(int k = 1; k <= (2*i) - 1; k++){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day10/Q40.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+    char letter;
+
+    printf("Enter the last character of pattern: ");
+    scanf("%c", &letter);
+
+    for(char i = 'A'; i <= letter; i++){
+        for(int j = 1; j <= letter - i; j++){
+            printf("  ");
+        }
+
+        for(char k = 'A'; k <= i; k++){
+            printf("%c ", k);
+        }
+
+        for(char l = i - 1; l >= 'A'; l--){
+            printf("%c ", l);
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
