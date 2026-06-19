65908eeeb874a77c4feb8766ccb6357a3d1a6869
shreyagarg5309 <shreyagarg5309@gmail.com>
2026-06-19 12:32:35 +0530

    Add files via upload

---
+++ Day23/Q90.c
@@ -0,0 +1,32 @@
+//first repeating character
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0}, len;
+
+    printf("Enter the string: ");
+    gets(str);
+    len = strlen(str);
+
+    for (int i = 0; i < len; i++){
+        freq[str[i]]++; 
+    }
+
+    int found = 0;
+    
+    for (int i = 0; i < len; i++) {
+        if (freq[str[i]] > 1) {
+            printf("First repeating character: %c", str[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0){
+        printf("All characters are non repeating");
+    }
+    
+    return 0;
+}
