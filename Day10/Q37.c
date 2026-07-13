72b0ffb8e28bb6cb06a5854ce6447577db559481
shreyagarg5309 <shreyagarg5309@gmail.com>
2026-07-13 13:40:54 +0530

    Update Q37.c

--- Day10/Q37.c
+++ Day10/Q37.c
@@ -8,16 +8,18 @@ int main(){
     printf("Enter the no of rows: ");
     scanf("%d", &num);
 
+    int numSpace = row - 1, numStar = 1;
     for(int i = 1; i <= num; i++){
-        for(int j = 1; j <= num - i; j++){
+        for(int j = 1; j <= numSpace; j++){
             printf(" ");
         }
 
-        for(int k = 1; k <= (2*i) - 1; k++){
-            printf("*");
+        for(int j = 1; j <= numStar; j++){
+            printf("* ");
         }
-        
         printf("\n");
+        numStar += 2;
+        numSpace--;
     }
 
     return 0;
